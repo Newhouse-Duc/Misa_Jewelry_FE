b25a8bf4f318a3b05ad418df635af6c70ad03335
Newhouse-Duc <ngoanhduc9a@gmail.com>
2024-10-12 16:45:12 +0700

    Initial commit
    updatedotenv

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-    baseURL: process.env.NODEJS_BASE_URL
+    baseURL: process.env.NODEJS_BASE_URL || 'http://localhost:8888'
 });
 
 const token = localStorage.getItem('jwt');
